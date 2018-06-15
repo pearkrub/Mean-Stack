e605ab7ff40b3970f262a51d8b7332f0f627ad69
Praibool Namwong <praibool.piak@gmail.com>
2018-06-15 15:15:45 +0700

    mount path

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var connect = require('connect')
 
 var app = connect()
 
-app.use(function(req, res, next) {
+var logger = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('log')
+}
+
+var hello = function(req, res, next) {
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hi')
-})
+}
+app.use('/log',logger)
+app.use('/hello',hello)
+
 app.listen(3000)
 
 console.log('Server Running at http://localhost:3000')
