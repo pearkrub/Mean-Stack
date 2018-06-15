45eb56ddfb483716028a24cfdccf692e4423d643
Praibool Namwong <praibool.piak@gmail.com>
2018-06-15 14:59:38 +0700

    test middleware

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var connect = require('connect')
 
 var app = connect()
 
+app.use(function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hi')
+})
 app.listen(3000)
 
 console.log('Server Running at http://localhost:3000')
